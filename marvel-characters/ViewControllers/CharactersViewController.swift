c0199180c6b8e725ab36d1f99f4911efc69c8d9a
Luiza Garofalo <luiza.garofalo@gmail.com>
2018-05-15 15:58:28 +0100

    Add ViewController files

---
+++ marvel-characters/ViewControllers/CharactersViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CharactersViewController.swift
+//  marvel-characters
+//
+//  Created by Luiza Collado Garofalo on 15/05/18.
+//  Copyright © 2018 Luiza Garofalo. All rights reserved.
+//
+
+import UIKit
+
+class CharactersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
