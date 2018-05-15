7c51afb860def8c6d1faf0599a3308a8afdab195
Luiza Garofalo <luiza.garofalo@gmail.com>
2018-05-15 14:59:49 +0100

    Initial Commit

---
+++ marvel-characters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  marvel-characters
+//
+//  Created by Luiza Collado Garofalo on 15/05/18.
+//  Copyright © 2018 Luiza Garofalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
