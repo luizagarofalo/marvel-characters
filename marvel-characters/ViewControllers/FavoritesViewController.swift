a82b68a821e5588718a265006cf6e7dbea9c30a0
Luiza Garofalo <luiza.garofalo@gmail.com>
2018-05-19 18:38:20 +0100

    Update FavoritesViewController

--- marvel-characters/ViewControllers/FavoritesViewController.swift
+++ marvel-characters/ViewControllers/FavoritesViewController.swift
@@ -12,7 +12,36 @@ class FavoritesViewController: UIViewController {
 
     @IBOutlet weak var favoritesCollectionView: UICollectionView!
 
+    var favorites = ["Favorite 01", "Favorite 02", "Favorite 03", "Favorite 04"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        favoritesCollectionView.register(UINib(nibName: "CharactersCollectionViewCell", bundle: nil),
+                                          forCellWithReuseIdentifier: "CharactersCell")
+
+        let width = (view.frame.size.width - 50) / 2
+        let layout = (favoritesCollectionView.collectionViewLayout as? UICollectionViewFlowLayout)!
+        layout.itemSize = CGSize(width: width, height: width)
+    }
+}
+
+extension FavoritesViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return favorites.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CharactersCell",
+                                                            for: indexPath) as? CharactersCollectionViewCell else {
+                                                                return UICollectionViewCell()
+        }
+
+        if let label = cell.viewWithTag(100) as? UILabel {
+            label.text = favorites[indexPath.row]
+        }
+
+        return cell
     }
 }
